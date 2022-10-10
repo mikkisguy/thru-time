ca6992b70b23357039878f053496617d3410497b
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2022-10-10 18:39:05 +0300

    Added constants for styles

---
+++ frontend/src/shared/styles/constants.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  BLACK: { DEFAULT: "#0b0c10", LIGHT: "#1f2833" },
+  GRAY: { DEFAULT: "#2c3531", LIGHT: "#c5c6c7" },
+  TURQUOISE: { DEFAULT: "#45a29e", LIGHT: "#66fcf1", DARK: "#116466" },
+  CYAN: { DEFAULT: "#d1e8e2" },
+  PEACH: { DEFAULT: "#ffcb9a", DARK: "#d9b08c" },
+};
+
+export const FONTS = {
+  PRIMARY: "'Source Code Pro', monospace",
+  SECONDARY: "'DM Sans', sans-serif",
+};
