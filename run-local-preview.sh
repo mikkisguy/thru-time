e643b0fce4884d4fea6181c876c6c82f9333d2a9
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2022-10-10 21:51:08 +0300

    Changed tagging, updated compose files, removed local scripts

--- run-local-preview.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t thru-time-api-preview ./backend/
-docker build -t thru-time-frontend-preview ./frontend/
-
-docker-compose -f docker-compose.preview.yml -p thru-time-preview up -d
--- run-local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t thru-time-api ./backend/
-docker build -t thru-time-frontend ./frontend/
-
-docker-compose -p thru-time up -d
