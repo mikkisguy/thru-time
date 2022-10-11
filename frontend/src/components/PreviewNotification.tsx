4452af75826a5b0fea3747efa4473967e2c07b67
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2022-10-11 22:02:22 +0300

    Fixed formatting, debugging

--- frontend/src/components/PreviewNotification.tsx
+++ frontend/src/components/PreviewNotification.tsx
@@ -1,15 +1,26 @@
-import { format } from "date-fns";
+import { format, parseISO, add } from "date-fns";
 import styled from "styled-components";
 import { LATEST_COMMIT_SHA, UPDATED_ON } from "../shared/constants";
 import { FONTS, COLORS } from "../shared/styles/constants";
 
 const PreviewNotification = () => {
+  console.log("UPDATED_ON", UPDATED_ON);
+
+  const getUpdateDate = (): Date => {
+    if (UPDATED_ON) {
+      // Adding three hours because of Github Actions time difference
+      return add(parseISO(UPDATED_ON), { hours: 3 });
+    }
+
+    return new Date();
+  };
+
   return (
     <PreviewTopBar>
       <PreviewSite>Preview site</PreviewSite>
       <span>
         Updated
-        {format(UPDATED_ON ? UPDATED_ON : new Date(), " yyyy-MM-dd HH:mm:ss O")}
+        {format(getUpdateDate(), " yyyy-MM-dd HH:mm:ss O")}
       </span>
       <CommitLink
         href={`https://github.com/mikkisguy/thru-time/commit/${LATEST_COMMIT_SHA}`}
