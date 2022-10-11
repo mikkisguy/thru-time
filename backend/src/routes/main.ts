7577ad8b2e76ab417fff9b18435d69cc15834edd
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2022-10-11 19:26:44 +0300

    Main route to separate file, preview stuff

---
+++ backend/src/routes/main.ts
@@ -0,0 +1,21 @@
+import { format } from "date-fns";
+import express, { Response, Router, NextFunction } from "express";
+import { IS_PREVIEW } from "../shared/constants";
+
+const router: Router = express.Router();
+const date = format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
+router.get("/", (_, res: Response, next: NextFunction): void => {
+  try {
+    const responseJson = {
+      message: `${IS_PREVIEW ? "Preview " : ""}API for Mikkis.fi`,
+      updatedOn: date,
+    };
+
+    res.send(responseJson);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
