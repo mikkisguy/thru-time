543f7ca445276a86aab378fda47062dd5c257063
MikkisGuy <82242822+mikkisguy@users.noreply.github.com>
2024-05-29 22:08:50 +0300

    Added states to form and started login handling

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,19 +1,42 @@
 import { useTranslation } from "react-i18next";
 import { Button, ContentBox, Input, Label } from "../shared/Styled";
 import styled from "styled-components";
+import { useState } from "react";
 
 const Login = () => {
   const { t } = useTranslation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <main>
       <LoginHeader>{t("login.title")}</LoginHeader>
       <LoginBox>
-        <form>
+        <form onSubmit={(e) => handleLogin(e)}>
           <Label htmlFor="username">{t("login.username")}</Label>
-          <Input type="text" id="username" name="username" autoComplete="username" required />
+          <Input
+            type="text"
+            id="username"
+            name="username"
+            autoComplete="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
           <Label htmlFor="password">{t("login.password")}</Label>
-          <Input type="password" id="password" name="password" autoComplete="current-password" required />
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
           <Button type="submit">{t("login.button")}</Button>
         </form>
       </LoginBox>
